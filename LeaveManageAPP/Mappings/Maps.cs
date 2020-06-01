1b4fc2d85e5ce43bb9b86c0572578d64ce568dbe
sunny <kvaidya221@gmail.com>
2020-06-01 18:39:02 +0530

    Add project files.

---
+++ LeaveManageAPP/Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LeaveManageAPP.Data;
+using LeaveManageAPP.Models;
+using LeaveManageAPP.Repository;
+
+namespace LeaveManageAPP.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
